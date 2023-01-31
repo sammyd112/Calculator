2cc924d215a4ea3f27adc5a8966bf28a52ab87c4
Samantha Dunlop <sammyd112@yahoo.com>
2023-01-30 21:17:08 -0500

    Inital and Final Commit

---
+++ arithmetic.py
@@ -0,0 +1,64 @@
+"""Functions for common math operations."""
+
+
+def add(num1, num2):
+    """Return the value of num1 and num2"""
+    total = num1 + num2
+
+    return total
+
+
+def subtract(num1, num2):
+    """Return the value of num1 minus num2."""
+    total = num1 - num2
+
+    return total
+
+def multiply(num1, num2):
+    """Multiply the num1 by num2 and return the result."""
+
+    total = num1 * num2
+
+    return total
+
+
+def divide(num1, num2):
+    """Divide the num1 by num2, returning a floating point."""
+
+    total = num1 / num2
+
+    return total
+
+
+def square(num1):
+    """Return the square of num1."""
+
+    total = num1 ** 2
+
+    return total
+
+
+
+
+def cube(num1):
+    """Return the cube of num1."""
+
+    total = num1 ** 3
+
+    return total
+
+
+def power(num1, num2):
+    """Raise num1 to the power of num2 and return the value."""
+
+    total = num1 ** num2
+
+    return total
+
+
+def mod(num1, num2):
+    """Return the remainder of num1 / num2."""
+
+    total = num1 % num2
+
+    return total
